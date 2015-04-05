bfe5b85d5acb138b83aba0d774ebeed642e15531
Duilio Palacios <i@duilio.me>
2015-04-05 21:10:30 +0000

    adding onDelete option to constraints on vote and comment tables

--- database/migrations/2015_04_04_203252_create_ticket_votes_table.php
+++ database/migrations/2015_04_04_203252_create_ticket_votes_table.php
@@ -17,10 +17,10 @@ class CreateTicketVotesTable extends Migration {
 			$table->increments('id');
 
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->integer('ticket_id')->unsigned();
-            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
 
 			$table->timestamps();
 		});
--- database/migrations/2015_04_04_230217_create_ticket_comments_table.php
+++ database/migrations/2015_04_04_230217_create_ticket_comments_table.php
@@ -20,10 +20,10 @@ class CreateTicketCommentsTable extends Migration {
             $table->string('link')->nullable();
 
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->integer('ticket_id')->unsigned();
-            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
 
 			$table->timestamps();
 		});
