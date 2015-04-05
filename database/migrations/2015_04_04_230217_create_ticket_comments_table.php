e0563309a68fcf42f7ae31928beec1d9c5292324
Duilio Palacios <i@duilio.me>
2015-04-05 17:15:48 +0000

    Adding the TicketComment new model and migration

---
+++ database/migrations/2015_04_04_230217_create_ticket_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketCommentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ticket_comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+            $table->mediumText('comment');
+            $table->string('link')->nullable();
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('ticket_id')->unsigned();
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('ticket_comments');
+	}
+
+}
