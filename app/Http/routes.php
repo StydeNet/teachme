a86bdf4f47eb4981ddbddc636f57b18931881d7d
vagrant <vagrant@homestead>
2015-04-10 02:08:52 +0000

    Revert "Public routes and Controller action placeholders"
    This reverts commit 10d6af069c63fb12884df6a615534d76aa85a9ee.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,30 +11,9 @@
 |
 */
 
-Route::get('/', [
-    'as'   => 'tickets.latest',
-    'uses' => 'TicketsController@latest'
-]);
+Route::get('/', 'WelcomeController@index');
 
-Route::get('/populares', [
-    'as'   => 'tickets.popular',
-    'uses' => 'TicketsController@popular'
-]);
-
-Route::get('/pendientes', [
-    'as'   => 'tickets.open',
-    'uses' => 'TicketsController@open'
-]);
-
-Route::get('/tutoriales', [
-    'as'   => 'tickets.closed',
-    'uses' => 'TicketsController@closed'
-]);
-
-Route::get('/solicitud/{id}', [
-    'as'   => 'tickets.details',
-    'uses' => 'TicketsController@details'
-]);
+Route::get('home', 'HomeController@index');
 
 Route::controllers([
     'auth' => 'Auth\AuthController',
