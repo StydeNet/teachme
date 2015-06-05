94f945a1c1a8dbc7fd5ebaadeff25ef8405c481c
vagrant <vagrant@homestead>
2015-06-05 14:35:11 +0000

    fixing bug of missing csrf_field function (in case you are using Laravel 5.0)

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,11 @@ function currentUser()
 {
     return auth()->user();
 }
+
+if ( ! function_exists('csrf_field'))
+{
+    function csrf_field()
+    {
+        return '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+    }
+}
