629355a9c38b0ca6cfdeed187bf8cefe423fe315
Duilio <silence@dessigual.com>
2015-06-05 14:36:03 -0430

    Base Repository (basic)

--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -4,7 +4,7 @@ namespace TeachMe\Repositories;
 
 use TeachMe\Entities\Ticket;
 
-class TicketRepository {
+class TicketRepository extends BaseRepository {
 
     public function getModel()
     {
@@ -13,7 +13,7 @@ class TicketRepository {
 
     protected function selectTicketsList()
     {
-        return Ticket::selectRaw(
+        return $this->newQuery()->selectRaw(
             'tickets.*, '
             . '( SELECT COUNT(*) FROM ticket_comments WHERE ticket_comments.ticket_id = tickets.id ) as num_comments,'
             . '( SELECT COUNT(*) FROM ticket_votes WHERE ticket_votes.ticket_id = tickets.id ) as num_votes'
@@ -41,11 +41,6 @@ class TicketRepository {
             ->where('status', 'closed')
             ->orderBy('created_at', 'DESC')
             ->paginate(20);
-    }
-
-    public function findOrFail($id)
-    {
-        return Ticket::findOrFail($id);
     }
 
 }
