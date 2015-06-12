38976d2f16ecdcd27e7536c315df68fe8fe79c31
Duilio <silence@dessigual.com>
2015-06-12 17:21:08 -0430

    JS vote function refactored and unvote functionality added

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,36 +2,59 @@ $(document).ready(function () {
 
     var alert = new Alert('#notifications');
 
-    $('.btn-vote').click(function (e) {
-        e.preventDefault();
+    function VoteForm(form, button, buttonRevert) {
 
-        var form = $('#form-vote');
-
-        var button = $(this);
         var ticket = button.closest('.ticket');
         var id = ticket.data('id');
-
         var action = form.attr('action').replace(':id', id);
+        var voteCount = ticket.find('.votes-count');
+
+        buttonRevert = ticket.find(buttonRevert);
 
         button.addClass('hidden');
 
-        $.post(action, form.serialize(), function (response) {
-            //alert
-            //update count votes
-            ticket.find('.btn-unvote').removeClass('hidden');
+        this.getVotes = function () {
+            return parseInt(voteCount.text().split(' ')[0]);
+        };
 
-            alert.success('¡Gracias por tu voto!');
+        this.updateCount = function (votes) {
+            voteCount.text(votes == 1 ? '1 voto' : votes + ' votos');
+        };
 
-            var voteCount = ticket.find('.votes-count');
-            var votos = parseInt(voteCount.text().split(' ')[0]);
-            votos++;
-            voteCount.text(votos == 1 ? '1 voto' : votos + ' votos');
+        this.submit = function (success) {
+            $.post(action, form.serialize(), function (response) {
+                buttonRevert.removeClass('hidden');
+                success(response);
+            }).fail(function () {
+                button.removeClass('hidden');
+                alert.error('Ocurrió un error :(');
+            });
+        };
+    }
 
-        }).fail(function () {
-            //print error message
-            button.removeClass('hidden');
+    $('.btn-vote').click(function (e) {
+        e.preventDefault();
 
-            alert.error('Ocurrió un error :(');
+        var voteForm = new VoteForm($('#form-vote'), $(this), '.btn-unvote');
+
+        voteForm.submit(function (response) {
+            if (response.success) {
+                alert.success('¡Gracias por tu voto!');
+                voteForm.updateCount(voteForm.getVotes() + 1);
+            }
+        });
+    });
+
+    $('.btn-unvote').click(function (e) {
+        e.preventDefault();
+
+        var voteForm = new VoteForm($('#form-unvote'), $(this), '.btn-vote');
+
+        voteForm.submit(function (response) {
+            if (response.success) {
+                alert.info('Voto eliminado');
+                voteForm.updateCount(voteForm.getVotes() - 1);
+            }
         });
     });
 
