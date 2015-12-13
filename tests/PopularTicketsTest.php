09c7c336ea225e5e25ce132de3b22b625f3ad32e
Duilio Palacios <i@duilio.me>
2015-12-13 16:32:02 +0000

    Add test for the popular tickets section

---
+++ tests/PopularTicketsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PopularTicketsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_see_popular_tickets()
+    {
+        $popularTicket = seed('Ticket');
+        $ticket = seed('Ticket');
+
+        seed('TicketVote', 10, ['ticket_id' => $popularTicket->id]);
+        seed('TicketVote', 2, ['ticket_id' => $ticket->id]);
+
+        $this->visit('/')
+            ->click('Populares')
+            ->seeInElement('h1', 'Solicitudes populares')
+            ->see($popularTicket->title)
+            ->see('10 votos')
+            ->dontSee($ticket->title)
+            ->dontSee('2 votos');
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,13 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 	/**
+	 * The base URL to use while testing the application.
+	 *
+	 * @var string
+	 */
+	protected $baseUrl = 'http://teach.app';
+
+	/**
 	 * Creates the application.
 	 *
 	 * @return \Illuminate\Foundation\Application
