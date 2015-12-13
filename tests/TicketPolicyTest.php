6cadab9986e244e66ad277a2906f73b8045d197a
Duilio Palacios <i@duilio.me>
2015-12-13 15:14:06 +0000

    Test scenario when a user or admin attempts to select a resource more than one time

--- tests/TicketPolicyTest.php
+++ tests/TicketPolicyTest.php
@@ -32,6 +32,19 @@ class TicketPolicyTest extends TestCase
         $this->assertFalse($policy->selectResource($otherUser, $ticket));
     }
 
+    public function test_prevent_users_for_selecting_a_resource_twice()
+    {
+        $user = seed('User');
+        $ticket = seed('Ticket', [
+            'user_id' => $user->id,
+            'status'  => 'closed',
+        ]);
+
+        $policy = new TicketPolicy();
+
+        $this->assertFalse($policy->selectResource($user, $ticket));
+    }
+
     public function test_administrators_can_select_resource()
     {
         $user = seed('User');
@@ -45,6 +58,43 @@ class TicketPolicyTest extends TestCase
 
         $this->assertTrue($admin->can('selectResource', $ticket));
         //$this->assertTrue(Gate::forUser($admin)->allows('selectResource', $ticket));
+    }
+
+    public function test_administrator_select_two_resources()
+    {
+        // Having
+        $user = seed('User', ['role' => 'admin']);
+        $ticket = seed('Ticket', ['user_id' => $user->id]);
+
+        $comment1 = seed('TicketComment', [
+            'ticket_id' => $ticket->id,
+            'link'      => 'https://styde.net'
+        ]);
+
+        $comment2 = seed('TicketComment', [
+            'ticket_id' => $ticket->id,
+            'link'      => 'https://twitter.com/StydeNet'
+        ]);
+
+        // When
+        $ticket->assignResource($comment1);
+        $ticket->assignResource($comment2);
+
+        // Then
+        $this->seeInDatabase('tickets', [
+            'id'   => $ticket->id,
+            'link' => 'https://twitter.com/StydeNet'
+        ]);
+
+        $this->seeInDatabase('ticket_comments', [
+            'id'       => $comment1->id,
+            'selected' => false,
+        ]);
+
+        $this->seeInDatabase('ticket_comments', [
+            'id'       => $comment2->id,
+            'selected' => true,
+        ]);
     }
 
 }
